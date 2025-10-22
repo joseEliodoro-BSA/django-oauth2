3c76b9d229b84aa6f233025441a657f1b5b65f78
Jose Eliodoro <jose.santana@bsatech.io>
2025-10-22 17:06:28 -0300

    feat: implementa rota e template de membros

--- tech/views.py
+++ tech/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def members(request):
+    return render(request, 'members.html')
